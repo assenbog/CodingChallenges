1e17673f09848dd8ae34b825abc8a3959848de33
Assen Bogdanov <assenb@hotmail.com>
2019-04-26 12:43:56 +0100

    Add to source control

---
+++ MostCommonValue/MostCommonValue/AnalyserTests.cs
@@ -0,0 +1,18 @@
+﻿namespace MostCommonValue
+{
+    using NUnit.Framework;
+
+    [TestFixture]
+    public class AnalyserTests
+    {
+        [Test]
+        public void IntTest()
+        {
+            var testArray = new[] { 3, 4, 5, 3, 3, 1, 3, 7, 8, 3, 2 };
+
+            var mostFrequent = Analyser.GetMostCommon<int>(testArray);
+
+            Assert.AreEqual(mostFrequent.Item1, 3);
+        }
+    }
+}
---
+++ MostCommonValue/MostCommonValue/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace MostCommonValue
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var testIntArray = new[] { 3, 4, 5, 3, 3, 1, 3, 7, 8, 3, 2 };
+
+            var mostFrequentInt = Analyser.GetMostCommon<int>(testIntArray);
+
+            var testStringArray = new[] { "Assen", "Valia", "Iana", "Tsveta", "Alex", "Assen", "Valia", "Assen" };
+
+            var mostFrequentString = Analyser.GetMostCommon<string>(testStringArray);
+        }
+    }
+}
