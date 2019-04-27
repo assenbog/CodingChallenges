c1fd46c5aec052f4c898479220727f8cc6433be7
Assen Bogdanov <assenb@hotmail.com>
2019-04-27 10:02:58 +0100

    Added to source control

---
+++ MostCommonValue/MostCommonValueNetFramework/MostCommonValueNetFramework/Analyser.cs
@@ -0,0 +1,19 @@
+﻿namespace MostCommonValue
+{
+    using System;
+    using System.Linq;
+    using System.Collections.Generic;
+
+    public static class Analyser
+    {
+        public static Tuple<T,int> GetMostCommon<T>(IEnumerable<T> items)
+        {
+            var itemGroupCount = from item in items
+                                 orderby item
+                                 group item by item into itemGroups
+                                 select new Tuple<T, int>(itemGroups.Key, itemGroups.Count());
+
+            return itemGroupCount.OrderByDescending(p => p.Item2).First();
+        }
+    }
+}
---
+++ MostCommonValue/MostCommonValueNetFramework/MostCommonValueNetFramework/AnalyserTests.cs
@@ -0,0 +1,48 @@
+﻿namespace MostCommonValue
+{
+    using NUnit.Framework;
+
+    [TestFixture]
+    public class AnalyserTests
+    {
+        [Test]
+        public void IntTest()
+        {
+            var testArray = new[] { 3, 4, 5, 3, 3, 1, 3, 7, 8, 3, 2 };
+
+            var mostFrequent = Analyser.GetMostCommon<int>(testArray);
+
+            Assert.AreEqual(mostFrequent.Item1, 3);
+        }
+
+        [Test]
+        public void StringTest()
+        {
+            var testArray = new[] { "Assen", "Valia", "Iana", "Tsveta", "Alex", "Assen", "Valia", "Assen" };
+
+            var mostFrequent = Analyser.GetMostCommon<string>(testArray);
+
+            Assert.AreEqual(mostFrequent.Item1, "Assen");
+        }
+
+        [Test]
+        public void CharTest()
+        {
+            var testArray = new[] { 'A', 'V', 'I', 'T', 'A', 'V', 'A' };
+
+            var mostFrequent = Analyser.GetMostCommon<char>(testArray);
+
+            Assert.AreEqual(mostFrequent.Item1, 'A');
+        }
+
+        [Test]
+        public void DoubleTest()
+        {
+            var testArray = new[] { 1.2D, 3.4D, 5D, 2D, 1.2D };
+
+            var mostFrequent = Analyser.GetMostCommon<double>(testArray);
+
+            Assert.AreEqual(mostFrequent.Item1, 1.2D);
+        }
+    }
+}
